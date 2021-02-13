0b325c18590c598f0dc745f3578b2839b5764cc4
Arunim singhal <75484060+arunim-iiitlCS@users.noreply.github.com>
2021-02-13 20:07:00 +0530

    A LoginApp Created by Arunim Singhal.

---
+++ app/src/main/java/com/example/neoproflearning/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.neoproflearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+    private TextView eInfo;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
